767c2e499e517b921d6d2bfe339786afb9a81839
lrkkevin <lrkkevin@163.com>
2019-01-26 17:48:49 +0800

    增加字段

--- DB_ZL/201901/20190126-01-刘瑞奎-增加字段.sql
+++ DB_ZL/201901/20190126-01-刘瑞奎-增加字段.sql
@@ -3,3 +3,11 @@ alter table cas.organ_info modify column ORGAN_PHONE VARCHAR(32) comment '企业
 
 alter table cas.organ_info add  ORGAN_EMAIL VARCHAR(32);
 alter table cas.organ_info modify column ORGAN_EMAIL VARCHAR(32) comment '企业邮箱';
+
+
+
+alter table CAS.CUST add  DOCKING_PEOPLE VARCHAR(32);
+alter table CAS.CUST modify column DOCKING_PEOPLE VARCHAR(32) comment '所属招商对接人';
+
+alter table CAS.CUST add  APP_DOCKING_PEOPLE VARCHAR(32);
+alter table CAS.CUST modify column APP_DOCKING_PEOPLE VARCHAR(32) comment '拟变更招商对接人';
